20918edcdc74bb3c57d0bf0cf037d57459bd1070
Sergey Veselovskiy <chimpook@gmail.com>
2019-10-07 14:34:28 +0300

    16. Loading the controller from the URL

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -5,15 +5,42 @@
  * URL Format: /controller/method/params
  */
 class Core {
-    protected $currentController = 'Pages';
-    protected $currentMethod = 'index';
+    protected $currentControllerName = 'Pages';
+    protected $currentMethodName = 'index';
     protected $params = [];
+    protected $currentController;
+    protected $currentMethod;
 
     public function __construct() {
-        $this->getUrl();
+        // print_r($this->getUrl());
+
+        $url = $this->getUrl();
+
+        // Look in controllers for first value
+        if (file_exists('../app/controllers/' 
+            . ucwords($url[0]) . '.php')) {
+            // If exists, set as controller
+            $this->currentControllerName = ucwords($url[0]);
+            
+            // Unset 0 Index
+            unset($url[0]);
+        }
+
+        // Require the controller
+        require_once '../app/controllers/' 
+            . $this->currentControllerName . '.php';
+
+        // Instantiate controller class
+        $this->currentController = new $this->currentControllerName;
     }
 
     public function getUrl() {
-        echo $_GET['url'];
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+
+            return $url;
+        }
     }
 }
