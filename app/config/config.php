65de7e5be86c8a9ea13eb91a23334e328ca4c874
Sergey Veselovskiy <chimpook@gmail.com>
2019-10-10 22:18:58 +0300

    23. The database class - part 1

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,10 @@
 <?php
+// DB parameters
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '123456');
+define('DB_NAME', 'tmvc');
+
 // App root
 define('APPROOT', dirname(dirname(__FILE__)));
 
