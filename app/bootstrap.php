75639b704ce4959f3159a6222fd57467ed211528
Sergey Veselovskiy <chimpook@gmail.com>
2019-10-10 09:21:31 +0300

    20. Config file & uploader. Use autoloader.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,7 +2,7 @@
 // Load Config
 require_once 'config/config.php';
 
-// Load libraries
-require_once 'libraries/Core.php';
-require_once 'libraries/Controller.php';
-require_once 'libraries/Database.php';
+// Autoload core libraries
+spl_autoload_register(function($className) {
+    require_once 'libraries/' . $className . '.php';
+});
